4fd2bb627c33111b3dfebead08224f5d73a891f0
danielOuattara <daniel.ouattara@gmx.com>
2022-12-20 18:53:39 +0100

    optimize server + correcting import & code

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,6 @@ const xss = require("xss-clean");
 const express = require("express");
 const app = express();
 
-const connectDB = require("./db/connect");
 const authenticateUser = require("./middleware/authentication");
 // routers
 const authRouter = require("./routes/authRoutes");
@@ -39,20 +38,4 @@ app.get("*", (req, res) => {
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-const port = process.env.PORT || 5000;
-
-const start = async () => {
-  try {
-    await connectDB(process.env.MONGO_URI);
-    console.log(
-      `Connection to MongoDB database ${process.env.MONGO_DATABASE} Success !`,
-    );
-    app.listen(port, () =>
-      console.log(`Server is listening on port http://localhost:${port}/`),
-    );
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-start();
+module.exports = app;
